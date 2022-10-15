f7cff3a06bb10272c2a0bd3fee8ad6da06b521fe
Greg Delgado <grd888@gmail.com>
2022-10-15 11:39:51 +0800

    test initialize account with multiple currencies

--- Currency ExchangeTests/UserAccountTests.swift
+++ Currency ExchangeTests/UserAccountTests.swift
@@ -21,6 +21,20 @@ final class UserAccountTests: XCTestCase {
     XCTAssertEqual(sut.balance[.JPY], 0.0)
   }
 
+  func test_initializeWithTwoCurrencies_returnsCorrectInitialBalances() {
+    let sut = UserAccount(initialBalance: [.EUR: 1_000, .JPY: 2_000_000])
+    XCTAssertEqual(sut.balance[.EUR], 1000)
+    XCTAssertEqual(sut.balance[.USD], 0.0)
+    XCTAssertEqual(sut.balance[.JPY], 2000000)
+  }
+
+  func test_initializeWithThreeCurrencies_returnsCorrectInitialBalances() {
+    let sut = UserAccount(initialBalance: [.EUR: 1_000, .JPY: 2_000_000, .USD: 3_000])
+    XCTAssertEqual(sut.balance[.EUR], 1000)
+    XCTAssertEqual(sut.balance[.USD], 3000)
+    XCTAssertEqual(sut.balance[.JPY], 2000000)
+  }
+
   // MARK: Helpers
   private func getDefaultBalance() -> AccountBalance {
     return Currency.allCases.reduce(into: AccountBalance()) { balance, currency in
