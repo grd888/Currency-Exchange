2d69f5932321209238bca5612d01d0d34449c6e0
Greg Delgado <grd888@gmail.com>
2022-10-15 15:07:58 +0800

    get current balances from view model

--- Currency Exchange/Currency Converter/CurrencyConverterViewModel.swift
+++ Currency Exchange/Currency Converter/CurrencyConverterViewModel.swift
@@ -5,20 +5,36 @@
 //  Created by Greg Delgado on 10/15/22.
 //
 
+import RxSwift
+import RxCocoa
 import Foundation
 
 class CurrencyConverterViewModel {
+  var currentBalanceObservable: Observable<AccountBalance> {
+    return currentBalance.asObservable()
+  }
+
   enum Section: Int, CaseIterable {
     case balances
     case converter
     case submit
   }
 
+  private var currentBalance: BehaviorRelay<AccountBalance>
+
   private var configuration: [Section: Int] = [
     .balances: 1,
     .converter: 2,
     .submit: 1
   ]
+
+  private var userAccount: UserAccount
+
+  init(userAccount: UserAccount = UserAccount()) {
+    self.userAccount = userAccount
+
+    currentBalance = BehaviorRelay(value: userAccount.balance)
+  }
 
   func numberOfSections() -> Int {
     return Section.allCases.count
--- Currency ExchangeTests/Currency Converter/CurrencyConverterViewModelTests.swift
+++ Currency ExchangeTests/Currency Converter/CurrencyConverterViewModelTests.swift
@@ -6,10 +6,25 @@
 //
 
 @testable import Currency_Exchange
+import RxSwift
+import RxCocoa
 import XCTest
 
 final class CurrencyConverterViewModelTests: XCTestCase {
   typealias Section = CurrencyConverterViewModel.Section
+  // swiftlint:disable:next implicitly_unwrapped_optional
+  var disposeBag: DisposeBag!
+
+  override func setUp() {
+    super.setUp()
+    disposeBag = DisposeBag()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    disposeBag = nil
+  }
+
 
   func test_numberOfSections() {
     XCTAssertEqual(makeSUT().numberOfSections(), 3)
@@ -30,10 +45,48 @@ final class CurrencyConverterViewModelTests: XCTestCase {
     XCTAssertEqual(makeSUT().numberOfRowsInSection(999), 0)
   }
 
+  func test_currentBalanceWithOneCurrencyInitialized() {
+    let account = UserAccount(initialBalance: [.EUR: 100000])
+    let sut = makeSUT(account)
+    let exp = expectation(description: "Waiting for observable...")
+    sut.currentBalanceObservable
+      .subscribe { balances in
+        XCTAssertEqual(account.balance[.EUR], balances[.EUR])
+        XCTAssertEqual(account.balance[.USD], 0)
+        XCTAssertEqual(account.balance[.JPY], 0)
+        exp.fulfill()
+      }
+      .disposed(by: disposeBag)
+    wait(for: [exp], timeout: 1.0)
+  }
+
+  func test_currentBalance() {
+    let account = UserAccount(initialBalance: [
+      .EUR: 100000,
+      .USD: 2000,
+      .JPY: 30000
+    ])
+    let sut = makeSUT(account)
+    let exp = expectation(description: "Waiting for observable...")
+    sut.currentBalanceObservable
+      .subscribe { balances in
+        XCTAssertEqual(account.balance[.EUR], balances[.EUR])
+        XCTAssertEqual(account.balance[.USD], balances[.USD])
+        XCTAssertEqual(account.balance[.JPY], balances[.JPY])
+        exp.fulfill()
+      }
+      .disposed(by: disposeBag)
+    wait(for: [exp], timeout: 1.0)
+  }
+
   // MARK: Helpers
 
-  private func makeSUT() -> CurrencyConverterViewModel {
-    let sut = CurrencyConverterViewModel()
+  private func makeSUT(_ account: UserAccount? = nil) -> CurrencyConverterViewModel {
+    let sut = CurrencyConverterViewModel(userAccount: account ?? makeUserAccount())
     return sut
+  }
+
+  private func makeUserAccount() -> UserAccount {
+    return UserAccount()
   }
 }
