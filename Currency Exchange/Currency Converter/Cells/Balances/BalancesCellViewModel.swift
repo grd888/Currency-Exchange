73f106a0e87c27203f6e15e718f7b413c6e3b50d
Greg Delgado <grd888@gmail.com>
2022-10-16 19:02:02 +0800

    use CurrencyFormatter to format currencies

--- Currency Exchange/Currency Converter/Cells/Balances/BalancesCellViewModel.swift
+++ Currency Exchange/Currency Converter/Cells/Balances/BalancesCellViewModel.swift
@@ -11,9 +11,11 @@ import Foundation
 
 struct BalancesCellViewModel {
   var currentBalance: BehaviorRelay<AccountBalance>
+  var formatter: CurrencyFormatting
 
-  init(currentBalanceObservable: BehaviorRelay<AccountBalance>) {
+  init(currentBalanceObservable: BehaviorRelay<AccountBalance>, formatter: CurrencyFormatting = CurrencyFormatter()) {
     self.currentBalance = currentBalanceObservable
+    self.formatter = formatter
   }
 
   func numberOfItems() -> Int {
@@ -24,7 +26,7 @@ struct BalancesCellViewModel {
     let allBalances = currentBalance.value.sorted(by: <)
     let balance = allBalances[index]
     let double = Double(truncating: balance.value as NSNumber)
-    let doubleString = balance.key.hasDecimal() ? String(format: "%.2f", double) : String(format: "%.0f", double)
+    let doubleString = (try? formatter.format(amount: double, in: balance.key)) ?? "---"
     return "\(doubleString) \(balance.key.toString())"
   }
 }
